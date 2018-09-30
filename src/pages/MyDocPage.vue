dce193c41223f1697d4eed91d48408905a0b628a
Alex <yxfacw@163.com>
2018-09-30 17:46:53 +0800

    add md editor page

---
+++ src/pages/MyDocPage.vue
@@ -0,0 +1,21 @@
+<!--我的文档-->
+<template>
+  <q-page padding>
+    <!-- content -->
+    <marked-editor edit-text="# Hello Vue" previewer-only></marked-editor>
+  </q-page>
+</template>
+
+<script>
+  import {MarkedEditor} from '../components'
+
+  export default {
+    // name: 'PageName',
+    components: {
+      MarkedEditor
+    }
+  }
+</script>
+
+<style>
+</style>
